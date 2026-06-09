33abec53954039266ef5ba92071a66189a33e255
abdelhamid <abdelhamaidm868@gmail.com>
2026-06-09 17:58:57 +0300

    aaaaaaaaaaa

--- modules/pharmacy/pharmacy.service.js
+++ modules/pharmacy/pharmacy.service.js
@@ -327,79 +327,7 @@ export const getPharmacyOrders = (req, res) => {
   } catch (error) {
     return res
       .status(500)
-<<<<<<< HEAD
       .json({ success: true, message: error.message, stack: error.stack });
-=======
-      .json({ success: false, message: error.message, stack: error.stack });
-  }
-};
-
-// -------------------------------------------------------------------------------------
-
-export const deletemedicine = (req, res) => {
-  const pharmacy_id = req.pharmacy_data.id;
-  const { medicine_id, Quantity } = req.body;
-
-  const query = `
-    SELECT pharmacymedicine.Quantity 
-    FROM pharmacymedicine 
-    WHERE PharmacyId = ? AND MedicineId = ?
-  `;
-
-  db.execute(query, [pharmacy_id, medicine_id], (error, result) => {
-    if (error) return res.json({ msg: error.message });
-
-    if (result.length == 0) {
-      return res.status(404).json({ msg: "Medicine not found" });
-    }
-
-    const currentQuantity = result[0].Quantity;
-
-    // ❌ لو عايز يحذف أكتر من الموجود
-    if (Quantity > currentQuantity) {
-      return res.status(400).json({
-        msg: "you not have this Quantity of Medicine",
-      });
-    }
-
-    // ✅ حذف كله
-    if (Quantity == currentQuantity) {
-      const deleteQuery = `
-        DELETE FROM pharmacymedicine 
-        WHERE PharmacyId = ? AND MedicineId = ?
-      `;
-
-      return db.execute(deleteQuery, [pharmacy_id, medicine_id], (err) => {
-        if (err) return res.json({ msg: err.message });
-
-        return res.status(200).json({
-          msg: "Delete all medicine Done",
-        });
-      });
-    }
-
-    // ✅ حذف جزء
-    const newQuantity = currentQuantity - Quantity;
-
-    const updateQuery = `
-      UPDATE pharmacymedicine 
-      SET Quantity = ? 
-      WHERE PharmacyId = ? AND MedicineId = ?
-    `;
-
-    db.execute(updateQuery, [newQuantity, pharmacy_id, medicine_id], (err) => {
-      if (err) return res.json({ msg: err.message });
-
-      res.status(200).json({
-        msg: "Delete part of medicine Done",
-        remaining: newQuantity,
-      });
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-      stack: error.stack,
-    });
->>>>>>> 11627e464ed80002d3c2d21619bf022dd6f24254
   }
 };
 
