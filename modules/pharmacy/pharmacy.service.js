ac8663ca15f1e5045e8600a3fe016203774323d1
abdelhamid <abdelhamaidm868@gmail.com>
2026-06-09 13:55:09 +0300

    aaaaaaaaaaa

--- modules/pharmacy/pharmacy.service.js
+++ modules/pharmacy/pharmacy.service.js
@@ -439,6 +439,39 @@ export const getall_medicine = (req, res) => {
     }
   });
 };
+export const delete_medicine = (req, res) => {
+  try {
+      const pharmacy_id = req.pharmacy_data.id;
+  const medicine_id = req.params.id;
+
+  const query = `
+    DELETE FROM pharmacymedicine
+    WHERE PharmacyId = ? AND MedicineId = ?
+  `;
+
+  db.execute(query, [pharmacy_id, medicine_id], (error, result) => {
+    if (error) {
+      return res.status(500).json({
+        msg: error.message,
+      });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        msg: "Medicine not found in this pharmacy",
+      });
+    }
+
+    res.status(200).json({
+      msg: "Medicine deleted successfully",
+    });
+  });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: error.message, stack: error.stack });
+  }
+};
 
 /////////////////////////////////////////////////////////////////
 
